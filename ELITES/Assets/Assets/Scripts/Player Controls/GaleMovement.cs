4e9d1fdaccb62f4717090e04f0c9f2eea7f35f6a
ziqs9 <32590070+ziqs9@users.noreply.github.com>
2018-07-31 13:16:17 +0800

    mapv3.1
    issue: door animation doesnt play

---
+++ ELITES/Assets/Assets/Scripts/Player Controls/GaleMovement.cs
@@ -0,0 +1,113 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GaleMovement : MonoBehaviour {
+
+    Animator animator;
+    public float moveSpeed = 1f;
+    //Cached components
+    private Rigidbody2D rigidBody;
+    private SpriteRenderer spriteRenderer;
+    private Transform myTransform;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rigidBody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        myTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movement();
+
+        {
+            // float movex = Input.GetAxis ("Horizontal");
+            // animator.SetFloat ("Speed", movex);
+            // float movez = Input.GetAxis ("Vertical");
+            // animator.SetFloat ("Speedz", movez);
+
+
+
+        }
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Animating(h, v);
+
+    }
+    void Movement()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            // rigidbody.AddForce(new Vector3 (1000.0f,0,0));
+            rigidBody.MovePosition(transform.position + transform.right * moveSpeed * Time.deltaTime);
+            // rigidBody.velocity = new Vector3 (moveSpeed, rigidBody.velocity.y, rigidBody.velocity.z);
+            // myTransform.rotation = Quaternion.Euler (0, 0, 0);
+            spriteRenderer.flipX = false;
+            // animator.SetBool("walk", true);
+
+            //transform.Translate(Vector3.right*0.75f*Time.deltaTime);
+            // var ForwardBack = false;
+            // transform.eulerAngles = new Vector3(0,0,0);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidBody.MovePosition(transform.position - transform.right * moveSpeed * Time.deltaTime);
+            // rigidBody.velocity = new Vector3 (-moveSpeed, rigidBody.velocity.y, rigidBody.velocity.z);
+            // myTransform.rotation = Quaternion.Euler (0, 0, 180);
+            spriteRenderer.flipX = true;
+            // animator.SetBool("walk", true);
+
+            //transform.Translate(Vector3.left*0.75f*Time.deltaTime);
+            // var ForwardBack = false;
+            // transform.eulerAngles = new Vector3(0,0,0);
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            rigidBody.MovePosition(transform.position + transform.up * moveSpeed * Time.deltaTime);
+            // animator.SetBool("walk", true);
+            // rigidBody.velocity = new Vector3 (rigidBody.velocity.x, rigidBody.velocity.y, moveSpeed);
+            // myTransform.rotation = Quaternion.Euler (0, 0, 0);
+
+            //transform.Translate(Vector3.forward*0.75f*Time.deltaTime);
+            // var ForwardBack = true;
+            // transform.eulerAngles = new Vector3(0,0,0);
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            rigidBody.MovePosition(transform.position - transform.up * moveSpeed * Time.deltaTime);
+            Debug.Log("forward");
+            // animator.SetBool("walk", true);
+            // rigidBody.velocity = new Vector3 (rigidBody.velocity.x, rigidBody.velocity.y, -moveSpeed);
+            // myTransform.rotation = Quaternion.Euler (0, 180, 0);
+
+            //transform.Translate(Vector3.back*0.75f*Time.deltaTime);
+            // var ForwardBack = true;
+            // transform.eulerAngles = new Vector3(0,0,0);
+        }
+
+    }
+
+    private void Animating(float h, float v)
+    {
+        bool walk = h != 0f || v != 0f;
+        animator.SetBool("walk", walk);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collided");
+        if (collision.gameObject.name == "Cube")  // or if(gameObject.CompareTag("YourWallTag"))
+        {
+            Debug.Log("Hit cube");
+            rigidBody.velocity = Vector3.zero;
+
+        }
+    }
+
+}
---
+++ ELITES/Assets/Assets/Scripts/World Interactions/OpenDoor.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenDoor : MonoBehaviour {
+
+    // private UnityEngine.Tilemaps.TilemapRenderer tiler;
+    Animation anim;
+    //var animationSwitch : boolean = false;
+    //var animateobject: GameObject;
+    
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animation>();
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log("Collided");
+        if (collision.gameObject.tag == "Player")  // or if(gameObject.CompareTag("YourWallTag"))
+        {
+            //Object.Destroy(tiler);
+            //animationSwitch = true;
+            //animateobject.animation.CrossFade("dooropen");
+            Debug.Log("Open Door");
+            //anim.Play("dooropen");
+            //if (!anim.isPlaying)
+            //{
+            //    Debug.Log("hi");
+            //    anim.Play("dooropen");
+            //}
+            PlayAnimation("dooropen");
+
+        }
+    }
+    void PlayAnimation(string AnimName)
+    {
+        if (!anim.IsPlaying(AnimName))
+        {
+            anim.CrossFadeQueued(AnimName, 0.3f, QueueMode.PlayNow);
+            Debug.Log("crossfade");
+        }
+    }
+}
