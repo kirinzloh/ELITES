60adb75ff8e4d62c8e0a3f1c4a0f0eff455b39da
Ooi Kai Lue <kailueooi@gmail.com>
2018-08-02 12:39:29 +0800

    added enemy

--- ELITES/Assets/Assets/Scripts/Agent Scripts/Enemy/EnemyAgentAttackScript.cs
+++ ELITES/Assets/Assets/Scripts/Agent Scripts/Enemy/EnemyAgentAttackScript.cs
@@ -4,13 +4,70 @@ using UnityEngine;
 
 public class EnemyAgentAttackScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    public float timeBetweenAttacks = 0.5f;
+    public int attackDamage = 10;
+    public Transform player;
+
+    //GameObject player;
+    //Animator anim;
+    //PlayerHealth playerHealth;
+    //EnemyHealth enemyHealth;
+    bool playerInRange;
+    float timer;
+    
+    void Start () {
+        //player = GameObject.FindGameObjectWithTag("Player");
+        //playerHealth = player.GetComponent<PlayerHealth>();
+        //enemyHealth = GetComponent<EnemyHealth>();
+        //anim = GetComponent<Animator>();
+    }
 	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == player)
+        {
+            playerInRange = true;
+        }
+    }
+
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.transform == player)
+        {
+            playerInRange = false;
+            Debug.Log("player left");
+        }
+    }
+
+
+    void Update()
+    {
+        Debug.Log(playerInRange);
+        timer += Time.deltaTime;
+
+        if (timer >= timeBetweenAttacks && playerInRange/* && enemyHealth.currentHealth > 0*/)
+        {
+            Attack();
+        }
+
+        //if (playerHealth.currentHealth <= 0)
+        //{
+        //    anim.SetTrigger("PlayerDead");
+        //}
+    }
+
+
+    void Attack()
+    {
+        timer = 0f;
+
+        Debug.Log("Player Attacked");
+
+        //if (playerHealth.currentHealth > 0)
+        //{
+        //    playerHealth.TakeDamage(attackDamage);
+        //}
+    }
 }
--- ELITES/Assets/Assets/Scripts/Sprite Scripts/Player/PlayerSpriteMovementScript.cs
+++ ELITES/Assets/Assets/Scripts/Sprite Scripts/Player/PlayerSpriteMovementScript.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerSpriteScript : MonoBehaviour {
+public class PlayerSpriteMovementScript : MonoBehaviour {
 
     public Transform agent;
     public float yOffset;
