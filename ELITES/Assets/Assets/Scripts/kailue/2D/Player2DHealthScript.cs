f0aa686b97c371c3a4361eda87ce2cdbe6c831e5
Ooi Kai Lue <kailueooi@gmail.com>
2018-08-05 03:10:01 +0800

    Basic Gale

---
+++ ELITES/Assets/Assets/Scripts/kailue/2D/Player2DHealthScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2DHealthScript : MonoBehaviour {
+
+    public int startingHealth = 100;
+    public int currentHealth;
+    //public Image damageImage;
+    //public float flashSpeed = 5f;
+    //public Color flashColour = new Color(1f, 0f, 0f, 0.1f);
+
+    //Animator anim;
+    Player2DMovementScript playerMovement;
+    Player2DAttackScript playerAttacking;
+    bool isDead;
+    //bool damaged;
+
+
+    void Start()
+    {
+        //anim = GetComponent<Animator>();
+        playerMovement = GetComponent<Player2DMovementScript>();
+        playerAttacking = GetComponent<Player2DAttackScript>();
+        currentHealth = startingHealth;
+    }
+
+
+    void Update()
+    {
+        //if (damaged)
+        //{
+        //    damageImage.color = flashColour;
+        //}
+        //else
+        //{
+        //    damageImage.color = Color.Lerp(damageImage.color, Color.clear, flashSpeed * Time.deltaTime);
+        //}
+        //damaged = false;
+    }
+
+
+    public void TakeDamage(int amount)
+    {
+        //damaged = true;
+        currentHealth -= amount;
+
+        if (currentHealth <= 0 && !isDead)
+        {
+            Death();
+        }
+    }
+
+
+    void Death()
+    {
+        isDead = true;
+
+        playerMovement.enabled = false;
+        playerAttacking.enabled = false;
+
+        Debug.Log("Player Dead");
+    }
+}
---
+++ ELITES/Assets/Assets/Scripts/kailue/3D/PlayerAttackScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAttackScript : MonoBehaviour {
+
+    private float timeBetweenAttack = 0.50f;
+
+    Animator anim;
+    float timer;
+    
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+
+    }
+    
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if ((Input.GetButton("Fire1") || Input.GetButton("Fire2")) && timer >= timeBetweenAttack && Time.timeScale != 0)
+        {
+            anim.SetTrigger("Attack");
+            timer = 0f;
+        }
+    }
+}
