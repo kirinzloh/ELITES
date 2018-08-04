d5bdd2571bb91c57ea2fbabc26335a6a33acb484
Loh Wei Quan <weiquan7@hotmail.com>
2018-08-05 00:45:34 +0800

    Added dialogue system and status UI

---
+++ ELITES/Assets/Assets/Scripts/Manager Scripts/DialogueManager.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager Instance { get; set; }
+
+    public Text nameText;
+    public Text dialogueText;
+    public float text_delay;
+    public Animator animator;
+
+    private Queue<string> sentences;
+    
+    // Use this for initialization
+    void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            sentences = new Queue<string>();
+        }
+    }
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        animator.SetBool("IsOpen", true);
+    
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        dialogueText.text = sentence;
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence)
+        {
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(text_delay);
+        }
+    }
+
+    void EndDialogue()
+    {
+        animator.SetBool("IsOpen", false);
+     }
+
+}
