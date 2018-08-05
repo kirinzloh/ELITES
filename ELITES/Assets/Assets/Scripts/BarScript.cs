5783a0a4aef6bac00af58a7095b09af0bd6bd000
Loh Wei Quan <weiquan7@hotmail.com>
2018-08-05 22:14:39 +0800

    added bar script
    controls increase/decrease of hp and energy together with status bar ui

---
+++ ELITES/Assets/Assets/Scripts/BarScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class BarScript : MonoBehaviour {
+
+    [SerializeField]
+    private float HP;
+    [SerializeField]
+    private float Energy;
+    
+    [SerializeField]
+    private Image hpBar;
+    [SerializeField]
+    private Image energyBar;
+
+    private float maxHP = 100;
+    private float maxEnergy = 100;
+
+    // Use this for initialization
+	void Start ()
+    {
+		
+	} 
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        HandleBar();
+	}
+
+    private void HandleBar()
+    {
+        hpBar.fillAmount = Map(HP, maxHP);
+        energyBar.fillAmount = Map(Energy, maxEnergy);
+    }
+
+    private float Map(float current, float max)
+    {
+        return (current / max);
+    }
+}
