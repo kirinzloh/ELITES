495b2676b843d1c3cfc20b995d3c4ed813387a52
ziqs9 <32590070+ziqs9@users.noreply.github.com>
2018-08-05 23:48:33 +0800

    changed all map to 2d and added some transitions

--- ELITES/Assets/Assets/Scripts/Transitions/TransitionPoint.cs
+++ ELITES/Assets/Assets/Scripts/Transitions/TransitionPoint.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class TransitionPoint : MonoBehaviour
 {
-    private readonly Vector3[] locations;
-    [System.Serializable]
+    
+    
     // code from 2D Gamekit (Unity tutorial)
     public enum TransitionType
     {
@@ -21,32 +21,31 @@ public class TransitionPoint : MonoBehaviour
 
     public enum TransitionWhen
     {
-        ExternalCall, InteractPressed, OnTriggerEnter,
+        InteractPressed, OnTriggerEnter,
     }
 
     [Tooltip("This is the gameobject that will transition.  For example, the player.")]
     public GameObject transitioningGameObject;
+
     [Tooltip("Whether the transition will be within this scene, to a different zone or a non-gameplay scene.")]
     public TransitionType transitionType;
-    //[SceneName]
-    //public string newSceneName;
-    //[Tooltip("The tag of the SceneTransitionDestination script in the scene being transitioned to.")]
-    //public SceneTransitionDestination.DestinationTag transitionDestinationTag;
-    //[Tooltip("The destination in this scene that the transitioning gameobject will be teleported.")]
-    //public TransitionPoint destinationTransform;
-    [Tooltip("Which map to transport to")]
-    public TransitionLocation transitionLocation;
+    
+    [Tooltip("The destination in this scene that the transitioning gameobject will be teleported.")]
+    public TransitionPoint destinationTransform;
+
     [Tooltip("What should trigger the transition to start.")]
     public TransitionWhen transitionWhen;
+
     [Tooltip("The player will lose control when the transition happens but should the axis and button values reset to the default when control is lost.")]
     public bool resetInputValuesOnTransition = true;
+
     //[Tooltip("Is this transition only possible with specific items in the inventory?")]
     //public bool requiresInventoryCheck;
     //[Tooltip("The inventory to be checked.")]
     //public InventoryController inventoryController;
     //[Tooltip("The required items.")]
     //public InventoryController.InventoryChecker inventoryCheck;
-    private Vector3 destinationTransform;
+    //private Vector3 destinationTransform;
 
     bool m_TransitioningGameObjectPresent;
 
@@ -54,13 +53,7 @@ public class TransitionPoint : MonoBehaviour
     // Use this for initialization
     void Start()
     {
-        //if (transitionWhen == TransitionWhen.ExternalCall)
-        //    m_TransitioningGameObjectPresent = true;
-        
-        locations[0] = new Vector3(-0.16f,9.54f,0f);
-        locations[1] = new Vector3(-17.2f, 0.32f, 0.37f);
-        locations[2] = new Vector3(19.38f, 12.72f, 0f);
-
+    
 
     }
 
@@ -77,7 +70,7 @@ public class TransitionPoint : MonoBehaviour
         {
             if (Input.GetKey(KeyCode.E)) // (PlayerInput.Instance.Interact.Down)
             {
-                Debug.Log("Left click");
+                Debug.Log("Interact Open");
                 TransitionInternal();
             }
         }
@@ -118,24 +111,6 @@ public class TransitionPoint : MonoBehaviour
         //        return;
         //}
 
-        if (transitionLocation == TransitionLocation.HQ)
-        {
-            destinationTransform = locations[0];
-
-        }
-
-        if (transitionLocation == TransitionLocation.Village)
-        {
-            destinationTransform = new Vector3(-17.2f, 0.32f, 0.37f);
-
-        }
-
-        if (transitionLocation == TransitionLocation.OddballHouse)
-        {
-            destinationTransform = locations[2];
-
-        }
-
 
         if (transitionType == TransitionType.SameScene)
         {
@@ -153,14 +128,12 @@ public class TransitionPoint : MonoBehaviour
         if (!m_TransitioningGameObjectPresent)
             return;
 
-        if (transitionWhen == TransitionWhen.ExternalCall)
-            TransitionInternal();
     }
 
-    public static void Teleport(GameObject transitioningGameObject, Vector3 destination)
+    public static void Teleport(GameObject transitioningGameObject, TransitionPoint destination)
     {
         Debug.Log("teleporting");
-        transitioningGameObject.transform.position = destination;
+        transitioningGameObject.transform.position = destination.transform.position;
     }
 
 
