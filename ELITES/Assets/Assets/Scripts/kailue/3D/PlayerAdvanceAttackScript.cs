341c43a46bf25b758f7db68927a092f4a7a67ff2
Ooi Kai Lue <kailueooi@gmail.com>
2018-08-05 17:49:30 +0800

    added character prefab

---
+++ ELITES/Assets/Assets/Scripts/kailue/3D/PlayerAdvanceAttackScript.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAdvanceAttackScript : MonoBehaviour {
+
+    private float timeBetweenAttack = 0.50f;
+
+    Animator anim;
+    AttackCollider attack;
+    float timer;
+
+    void Start()
+    {
+        anim = GetComponentInChildren<Animator>();
+        attack = GetComponentInChildren<AttackCollider>();
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (Input.GetButton("Fire1") && timer >= timeBetweenAttack && Time.timeScale != 0)
+        {
+            anim.SetTrigger("AttackLeft");
+            attack.Attack();
+            timer = 0f;
+        }
+        else if (Input.GetButton("Fire2") && timer >= timeBetweenAttack && Time.timeScale != 0)
+        {
+            anim.SetTrigger("AttackRight");
+            attack.Attack();
+            timer = 0f;
+        }
+        else if (Input.GetKeyDown(KeyCode.Q) && timer >= timeBetweenAttack && Time.timeScale != 0)
+        {
+            anim.SetTrigger("TrueStrike");
+            timer = 0f;
+        }
+        else if (Input.GetKeyDown(KeyCode.E) && timer >= timeBetweenAttack && Time.timeScale != 0)
+        {
+            anim.SetTrigger("Cleave");
+            timer = 0f;
+        }
+        else if (Input.GetKeyDown(KeyCode.R) && timer >= timeBetweenAttack && Time.timeScale != 0)
+        {
+            anim.SetTrigger("Stun");
+            timer = 0f;
+        }
+        else if (Input.GetKeyDown(KeyCode.F) && timer >= timeBetweenAttack && Time.timeScale != 0)
+        {
+            //anim.SetTrigger("HyperSpeed");
+            timer = 0f;
+        }
+    }
+}
