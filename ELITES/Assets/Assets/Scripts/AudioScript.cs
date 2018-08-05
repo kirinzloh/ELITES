976b8d73d0fd152bbc9a37e6994c6b97734234fc
Loh Wei Quan <weiquan7@hotmail.com>
2018-08-06 00:11:50 +0800

    Added settings scene

---
+++ ELITES/Assets/Assets/Scripts/AudioScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AudioScript : MonoBehaviour {
+
+    [SerializeField]
+    private Image volumeBar;
+
+    private AudioSource audioSrc;
+
+    private float musicVolume = 1f;
+
+    void Start()
+    {
+        audioSrc = GameObject.FindGameObjectWithTag("Audio").GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        audioSrc.volume = musicVolume;
+    }
+
+
+    public void VolumeChange(float newVolume)
+    {
+        volumeBar.fillAmount = 1-newVolume;
+        musicVolume = volumeBar.fillAmount;
+    }
+
+}
